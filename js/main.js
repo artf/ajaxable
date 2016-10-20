7f9e64063eacf25a2e20a3e3fd712dc5b5cf85ef
Artur Arseniev <artur.catch@hotmail.it>
2016-10-20 15:33:52 +0200

    Update demos

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var $header = $('#page-header, #pjs-sect');
+  var viewportH = $(window).height();
+  var $root = $('html, body');
+  $header.css('min-height', viewportH);
+
+  // highlight.js init
+  //$('pre code').each(function(i, block) {
+    //hljs.highlightBlock(block);
+  //});
+
+  $('.scroll-link').click(function() {
+    console.log('clicked');
+    var target = $($.attr(this, 'data-target'));
+    if( target.length ){
+        $root.animate({
+            scrollTop: (target.offset().top -70 )
+        }, 500);
+        return false;
+    }
+  });
+
+})
